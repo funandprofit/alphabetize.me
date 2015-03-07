b97aff47e64d731f1f162d091aef4e73c7712897
Sebastian von Conrad <sebastian@vonconrad.com>
2015-03-07 13:39:27 +1100

    Meaningless commit

--- app.rb
+++ app.rb
@@ -9,7 +9,7 @@ set :app_file, __FILE__
 set :root, File.dirname(__FILE__)
 set :views, 'views'
 set :public, 'public'
-set :haml, {:format => :html5} # default Haml format is :xhtml
+set :haml, { :format => :html5 }
 
 get '/' do
   haml :index
